8f545192738ba5aab29a424587145b2a33af9ebb
Raphael Carvalho Torres <rt@targetit.com.br>
2018-09-26 17:46:57 -0300

    CadastroPJController

---
+++ src/main/java/com/rct/pontointeligente/api/controllers/CadastroPJController.java
@@ -0,0 +1,64 @@
+package com.rct.pontointeligente.api.controllers;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rct.pontointeligente.api.dtos.CadastroPJDto;
+import com.rct.pontointeligente.api.entities.Empresa;
+import com.rct.pontointeligente.api.entities.Funcionario;
+import com.rct.pontointeligente.api.response.Response;
+import com.rct.pontointeligente.api.services.EmpresaService;
+import com.rct.pontointeligente.api.services.FuncionarioService;
+
+@RestController
+@RequestMapping("/api/cadastrar-pj")
+@CrossOrigin(origins = "*")
+public class CadastroPJController {
+
+	private static final Logger log = LoggerFactory.getLogger(CadastroPJController.class);
+	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@Autowired
+	private EmpresaService empresaService;
+	
+	public CadastroPJController() {
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Response<CadastroPJDto>> cadastrar(@Valid @RequestBody CadastroPJDto cadastroPJDto,
+			BindingResult result) {
+		log.info("Cadastrando PJ: {}", cadastroPJDto.toString());
+		Response<CadastroPJDto> response = new Response<CadastroPJDto>();
+		
+		validarDadosExistentes(cadastroPJDto, result);
+		Empresa empresa = this.converterDtoParaEmpresa(cadastroPJDto);
+		Funcionario funcionario = this.converterDtoParaFuncionario(cadastroPJDto, result);
+		
+		if (result.hasErrors()) {
+			log.error("Erro validando dados de cadastro PJ: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		this.empresaService.persistir(empresa);
+		funcionario.setEmpresa(empresa);
+		this.funcionarioService.persistir(funcionario);
+		
+		response.setData(this.converterCadastroPJDto(funcionario));
+		return ResponseEntity.ok(response);
+	}
+	
+}
--- src/main/java/com/rct/pontointeligente/api/response/Response.java
+++ src/main/java/com/rct/pontointeligente/api/response/Response.java
@@ -1,5 +1,32 @@
 package com.rct.pontointeligente.api.response;
 
-public class Response {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Response<T> {
+
+	private T data;
+	private List<String> errors;
+
+	public Response() {
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public List<String> getErrors() {
+		if (this.errors == null) {
+			this.errors = new ArrayList<String>();
+		}
+		return errors;
+	}
+
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
 }
