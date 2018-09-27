e9daa0d96132fcd33ae8089f3a85427998b68d08
Raphael Carvalho Torres <rt@targetit.com.br>
2018-09-27 10:55:40 -0300

    Cadastro de pessoa física

--- src/main/java/com/rct/pontointeligente/api/controllers/CadastroPJController.java
+++ src/main/java/com/rct/pontointeligente/api/controllers/CadastroPJController.java
@@ -1,5 +1,7 @@
 package com.rct.pontointeligente.api.controllers;
 
+import java.security.NoSuchAlgorithmException;
+
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
@@ -7,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rct.pontointeligente.api.dtos.CadastroPJDto;
 import com.rct.pontointeligente.api.entities.Empresa;
 import com.rct.pontointeligente.api.entities.Funcionario;
+import com.rct.pontointeligente.api.enums.PerfilEnum;
 import com.rct.pontointeligente.api.response.Response;
 import com.rct.pontointeligente.api.services.EmpresaService;
 import com.rct.pontointeligente.api.services.FuncionarioService;
@@ -26,39 +30,116 @@ import com.rct.pontointeligente.api.services.FuncionarioService;
 public class CadastroPJController {
 
 	private static final Logger log = LoggerFactory.getLogger(CadastroPJController.class);
-	
+
 	@Autowired
 	private FuncionarioService funcionarioService;
-	
+
 	@Autowired
 	private EmpresaService empresaService;
-	
+
 	public CadastroPJController() {
-		
 	}
-	
+
+	/**
+	 * Cadastra uma pessoa jurídica no sistema
+	 * 
+	 * @param cadastroPJDto
+	 * @param result
+	 * @return ResponseEntity<Response<CadastroPJDto>>
+	 * @throws NoSuchAlgorithmException
+	 */
 	@PostMapping
 	public ResponseEntity<Response<CadastroPJDto>> cadastrar(@Valid @RequestBody CadastroPJDto cadastroPJDto,
-			BindingResult result) {
+			BindingResult result) throws NoSuchAlgorithmException {
+		// @Valid chama o beans validation para verificar as regras
+		// @RequestBody converte os dados recebidos para um objeto CadastroPJDto
 		log.info("Cadastrando PJ: {}", cadastroPJDto.toString());
 		Response<CadastroPJDto> response = new Response<CadastroPJDto>();
-		
+
 		validarDadosExistentes(cadastroPJDto, result);
 		Empresa empresa = this.converterDtoParaEmpresa(cadastroPJDto);
 		Funcionario funcionario = this.converterDtoParaFuncionario(cadastroPJDto, result);
-		
+
 		if (result.hasErrors()) {
 			log.error("Erro validando dados de cadastro PJ: {}", result.getAllErrors());
 			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
 			return ResponseEntity.badRequest().body(response);
 		}
-		
+
 		this.empresaService.persistir(empresa);
 		funcionario.setEmpresa(empresa);
 		this.funcionarioService.persistir(funcionario);
-		
+
 		response.setData(this.converterCadastroPJDto(funcionario));
 		return ResponseEntity.ok(response);
 	}
-	
+
+	/**
+	 * Verifica se a empresa ou funcionário já existe na base de dados
+	 * 
+	 * @param cadastroPJDto
+	 * @param result
+	 */
+	private void validarDadosExistentes(CadastroPJDto cadastroPJDto, BindingResult result) {
+		this.empresaService.buscarPorCnpj(cadastroPJDto.getCnpj())
+				.ifPresent(emp -> result.addError(new ObjectError("empresa", "Empresa já existente.")));
+
+		this.funcionarioService.buscarPorCpf(cadastroPJDto.getCpf())
+				.ifPresent(emp -> result.addError(new ObjectError("empresa", "CPF já existente.")));
+
+		this.funcionarioService.buscarPorEmail(cadastroPJDto.getEmail())
+				.ifPresent(emp -> result.addError(new ObjectError("empresa", "Email já existente.")));
+	}
+
+	/**
+	 * Popula o DTO de cadastro com os dados do funcionário e empresa
+	 * 
+	 * @param funcionario
+	 * @return CadastroPJDto
+	 */
+	private CadastroPJDto converterCadastroPJDto(Funcionario funcionario) {
+		CadastroPJDto cadastroPJDto = new CadastroPJDto();
+		cadastroPJDto.setId(funcionario.getId());
+		cadastroPJDto.setNome(funcionario.getNome());
+		cadastroPJDto.setEmail(funcionario.getEmail());
+		cadastroPJDto.setCpf(funcionario.getCpf());
+		cadastroPJDto.setRazaoSocial(funcionario.getEmpresa().getRazaoSocial());
+		cadastroPJDto.setCnpj(funcionario.getEmpresa().getCnpj());
+		
+		return cadastroPJDto;
+	}
+
+	/**
+	 * Converte os dados do DTO para funcionário
+	 * 
+	 * @param cadastroPJDto
+	 * @param result
+	 * @return Funcionario
+	 * @throws NoSuchAlgorithmException
+	 */
+	private Funcionario converterDtoParaFuncionario(CadastroPJDto cadastroPJDto, BindingResult result)
+			throws NoSuchAlgorithmException {
+		Funcionario funcionario = new Funcionario();
+		funcionario.setNome(cadastroPJDto.getNome());
+		funcionario.setEmail(cadastroPJDto.getEmail());
+		funcionario.setCpf(cadastroPJDto.getCpf());
+		funcionario.setPerfil(PerfilEnum.ROLE_ADMIN);
+		funcionario.setSenha(cadastroPJDto.getSenha());
+
+		return funcionario;
+	}
+
+	/**
+	 * Converte os dados do DTO para empresa
+	 * 
+	 * @param cadastroPJDto
+	 * @return Empresa
+	 */
+	private Empresa converterDtoParaEmpresa(CadastroPJDto cadastroPJDto) {
+		Empresa empresa = new Empresa();
+		empresa.setCnpj(cadastroPJDto.getCnpj());
+		empresa.setRazaoSocial(cadastroPJDto.getRazaoSocial());
+		
+		return empresa;
+	}
 }
